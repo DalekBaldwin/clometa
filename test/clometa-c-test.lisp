0e360038fd78b2fd5c2a0a638c4fb8a43072ddd1
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2015-02-05 21:40:49 -0800

    beginnings of compiled tests

--- test/clometa-c-test.lisp
+++ test/clometa-c-test.lisp
@@ -4,3 +4,25 @@
 
 (defsuite* test-all)
 
+(defgrammar std ()
+  (char-range (x y)
+              (bind c _)
+              :->? (and (characterp c)
+                        (char<= x c y))
+              :-> c)
+  (letter () (or (char-range #\a #\z)
+              (char-range #\A #\Z)))
+  (digit () (char-range #\0 #\9))
+  (num () (+ (digit)))
+  (spaces () (+ #\space)))
+
+(deftest test-letter ()
+  (is (equal (multiple-value-list (omatch std letter () (list #\a #\1)))
+             '(#\a (#\1)))))
+
+(defgrammar simple-binding ()
+  (start ()
+         (list
+          (bind x (atom 1))
+          (bind y (atom 2)))
+         (-> (list x y))))
